3701955885247fa8e737f476293a642e5de9684e
Micah Eckard <lumberjacksoul339@gmail.com>
2024-09-02 21:11:29 -0400

    added hover animation to plant list

--- src/Components/FlowerList.js
+++ src/Components/FlowerList.js
@@ -1,16 +1,9 @@
-
 import { plantinfo } from "../Plantinfo";
 import Flower from "./Flower";
 
 export default function FlowerList() {
-    const plants = plantinfo.map((plant) => {
-        return(
-            <Flower name={plant.name}/>
-        )
-    })
-		return (
-            <div className="plant-list">
-                {plants}
-            </div>
-        )
+	const plants = plantinfo.map((plant) => {
+		return <Flower name={plant.name} />;
+	});
+	return <div className="plant-list">{plants}</div>;
 }
